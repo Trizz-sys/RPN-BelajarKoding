cf6f406683ae0b1d2b811747e9140d3f8a950036
Trizz-sys <triselabagus03@gmail.com>
2024-06-23 21:12:51 +0700

    Kodingan 6.5

---
+++ looping_soal1.js
@@ -0,0 +1,23 @@
+// soal 1
+// 1. Melakukan Looping Menggunakan While
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while.
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+let number = 0;
+let number2 = 10;
+// Looping pertama hitungan maju
+console.log("Looping Pertama");
+while (number <= 10) {
+  console.log(number);
+  number++;
+}
+// Looping kedua hitungan mundur
+console.log("\n");
+console.log("Looping Kedua");
+while (number2 > 0) {
+  console.log(number2);
+  number2--;
+}
